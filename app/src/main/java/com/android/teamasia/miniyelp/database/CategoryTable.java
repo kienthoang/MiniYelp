82d539956f74f532a5c41dee69496017a5185d4f
Kien Hoang <kienhoangdev@gmail.com>
2014-09-20 21:58:00 -0400

    Working on the table classes. Adding CRUD.

--- app/src/main/java/com/android/teamasia/miniyelp/database/CategoryTable.java
+++ app/src/main/java/com/android/teamasia/miniyelp/database/CategoryTable.java
@@ -7,4 +7,6 @@ public class CategoryTable {
     public static final String TABLE_NAME = "categories";
     public static final String COLUMN_ID = "_id";
     public static final String COLUMN_NAME = "name";
+
+
 }
--- app/src/main/java/com/android/teamasia/miniyelp/database/RestaurantTable.java
+++ app/src/main/java/com/android/teamasia/miniyelp/database/RestaurantTable.java
@@ -1,5 +1,14 @@
 package com.android.teamasia.miniyelp.database;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
 /**
  * Created by kienhoang on 9/20/14.
  */
@@ -11,4 +20,7 @@ public class RestaurantTable {
     public static final String COLUMN_COST = "cost";
     public static final String COLUMN_STREET = "street";
     public static final String COLUMN_CITY = "city";
+
+    private SQLiteDatabase database;
+    private MiniYelpSQLiteHelper dbHelper;
 }
